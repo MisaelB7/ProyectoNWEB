88ac25b43b989c87fd707f49037e3cbc7aefa585
akumanam <nahumlop17@gmail.com>
2021-12-09 20:19:47 -0600

    a

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const APP = express();
 const PATH = require('path');
+const mysql = require('mysql');
+
 
 APP.set('port', 3000);
 
@@ -10,3 +12,15 @@ APP.listen(APP.get('port'), () => {
     console.log(`Aplicacion corriendo en el puerto ${APP.get('port')}`);
     console.log(__dirname);
 });
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Cisco0105',
+    database: 'FlaxNam'
+  });
+  
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("IT IS Connected");
+  });
